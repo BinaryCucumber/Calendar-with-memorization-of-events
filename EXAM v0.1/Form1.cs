f5f0deafaaf97da6b61c96d1b364f4fb2e8a920b
BinaryCucumber <121834915+BinaryCucumber@users.noreply.github.com>
2023-03-26 03:56:53 +0300

    Add files via upload

---
+++ EXAM v0.1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Specialized;
+
+namespace EXAM_v0._1
+{
+    public partial class Form1 : Form
+    {
+        private string CurrentDate;
+        NameValueCollection MyCol = new NameValueCollection();
+        private string path = @"C:\Users\Public\Preparing for exams v0.1\ColSave.txt";
+        private string folder = @"C:\Users\Public\Preparing for exams v0.1";
+        private bool flag;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+            if (!(flag = File.Exists(path)))
+            {
+                Directory.CreateDirectory(folder);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MyCol.Add(CurrentDate, textBox1.Text);
+            textBox1.Clear();
+            FileSave();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            if (MyCol.Count != 0)
+            {
+                for (int i = 0; i < MyCol.Count; ++i)
+                {
+                    if (CurrentDate == MyCol.GetKey(i))
+                    {
+                        string St = MyCol[i];
+                        string NewSt = "",DopSt = "";
+                        int n = 1;
+                        for (int j = 0; j < St.Length; j++)
+                        {
+                            if (j == St.Length - 1)
+                            {
+                                NewSt += n.ToString() + ") " + DopSt+ St[j];
+                            }
+                            if (St[j] == ',')
+                            {
+                                NewSt += n.ToString()+") "+ DopSt + "\n";
+                                n++; DopSt = "";
+                                continue;
+                            }
+                            DopSt += St[j];
+                        }
+                        MessageBox.Show(MyCol.GetKey(i) + "\n"+ Environment.NewLine  + NewSt);
+                        k++;
+                    }
+                }
+                if (k == 0) MessageBox.Show("На указанную дату не намечено никаких событий");
+            }
+            else MessageBox.Show("На указанную дату не намечено никаких событий");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show(
+           "Вы уверены, что хотите удалить все события всех дат?!",
+           "ПРЕДУПРЕЖДЕНИЕ",
+           MessageBoxButtons.YesNo
+           );
+            if (res == DialogResult.Yes)
+            {
+                MyCol.Clear();
+                File.Create(path).Close();
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show(
+           "Вы уверены, что хотите удалить все события выбранной даты??",
+           "ПРЕДУПРЕЖДЕНИЕ",
+           MessageBoxButtons.YesNo
+           );
+            if (res == DialogResult.Yes)
+            {
+                MyCol.Remove(CurrentDate);
+                FileSave();
+            }
+        }
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            CurrentDate = e.Start.ToLongDateString();
+        }
+        private void FileSave()
+        {
+            using (FileStream file = new FileStream(path,FileMode.Create))
+            {
+                using (StreamWriter Sw = new StreamWriter(file))
+                {
+                    for (int i = 0; i < MyCol.Count; ++i)
+                    {
+                        Sw.WriteLine(MyCol.GetKey(i) + "*" + MyCol[i]);
+                    }
+                }
+            }    
+        }
+        private void FileLoad()
+        {
+            string key = "", value = "", line;
+            int n = 0;
+            if(!flag) File.Create(path).Close();
+            string[] mas = File.ReadAllLines(path);
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[0] == "" || mas[0] == null)
+                {
+                    break;
+                }
+                line = mas[i];
+                for (int j = 0; j < line.Length-1; j++)
+                {
+                    if(j != 0)
+                    {
+                        if (line[j - 1] == '*')
+                        {
+                            n = j;
+                            break;
+                        }
+                    }
+                    if(line[j] != '*') key += line[j];
+                }
+                for (int l = n; l < line.Length; l++)
+                {
+                    value += line[l];
+                }
+                MyCol.Add(key, value);
+                key = ""; value = "";
+            }
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileLoad();
+        }
+
+    } 
+}
---
+++ EXAM v0.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("EXAM v0.2")]
+[assembly: AssemblyDescription("Приложение для подготовки к экзаменам")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EXAM v0.1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("78c04e7b-f80d-46c0-a678-af725c2da4c6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
